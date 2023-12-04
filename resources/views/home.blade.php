05f0d4c1d345e58a3c2901960044aa7928289385
AnthonioMasbroo <129573492+AnthonioMasbroo@users.noreply.github.com>
2023-12-04 16:40:50 +0700

    tambah AOS dan responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://fonts.cdnfonts.com/css/poetsen-one" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <!-- Styles -->
     <style>
         body {
@@ -134,22 +136,24 @@
             display: flex;
             justify-content: center;
             margin-top: 60px;
-            
+
         }
-        
+
         .card {
             padding: 30px;
             text-align: center;
             background-color: #FFFACD;
             box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
         }
-        .card img{
+
+        .card img {
             width: 40%;
             margin-left: 80px;
             margin-bottom: 40px;
-            
+
         }
-        .card h4{
+
+        .card h4 {
             font-weight: bold;
             font-family: 'Poppins', sans-serif;
         }
@@ -170,7 +174,7 @@
             display: flex;
             justify-content: center;
             margin-top: 60px;
-            
+
         }
 
         .beritalainnya {
@@ -206,6 +210,388 @@
             font-family: 'Poetsen One', sans-serif;
             font-size: 24px;
         }
+
+        /* RESPONSIVE HANDPHONE */
+
+        @media (max-width: 767px) and (min-width: 320px) {
+
+
+            .about {
+                display: flex;
+                justify-content: center;
+            }
+
+            .about .title {
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 16px;
+                margin-top: 200px;
+            }
+
+            .isiabout {
+                flex-direction: column;
+                place-items: center;
+                padding: 20px;
+            }
+
+            .isiabout .penjelasan {
+                width: 100%;
+                text-align: justify;
+                font-size: 12px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                margin-bottom: 20px;
+            }
+
+            .isiabout img {
+                width: 100%;
+                height: 200px;
+                border-radius: 10px;
+            }
+
+            .visi-misi .title {
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 16px;
+            }
+
+            .isivisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .visi {
+                padding: 50px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                background-color: rgba(255, 216, 117, 0.60);
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                text-align: justify;
+                margin-top: 40px;
+                border-radius: 15px;
+                font-size: 12px;
+            }
+
+            .visi h1 {
+                font-size: 14px;
+                font-family: 'Poetsen One', sans-serif;
+            }
+
+            .isimisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .misi {
+                padding: 50px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                background-color: #FFFACD;
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                text-align: justify;
+                margin-top: 40px;
+                border-radius: 15px;
+                float: right;
+                font-size: 12px;
+            }
+
+            .misi h1 {
+                font-size: 14px;
+                font-family: 'Poetsen One', sans-serif;
+                float: right;
+            }
+
+            .program .title {
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 16px;
+            }
+
+            .column {
+                text-align: center;
+                width: 60%;
+                height: 40%;
+                padding: 20px;
+            }
+
+            .programunggulan {
+                margin: 0;
+                display: flex;
+                place-items: center;
+                margin-top: 60px;
+
+            }
+
+            .card {
+                padding: 30px;
+                text-align: center;
+                background-color: #FFFACD;
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                border-radius: 60px;
+            }
+
+            .card img {
+                width: 50%;
+                margin-left: 25px;
+                margin-bottom: 10px;
+
+            }
+
+            .card h4 {
+                font-weight: bold;
+                font-family: 'Poppins', sans-serif;
+                font-size: 14px;
+            }
+
+            .berita .title {
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 16px;
+            }
+
+            .kartuberita {
+                margin: 0;
+                display: grid;
+                place-items: center;
+                margin-top: 60px;
+
+            }
+
+            .kartuberita .column {
+                width: 100%;
+            }
+
+            .beritalainnya {
+                display: flex;
+                justify-content: center;
+                margin-top: 80px;
+            }
+
+            .beritalainnya a {
+                padding: 15px 10px;
+                border: none;
+                border-radius: 15px;
+                background-color: #FFD875;
+                color: black;
+                text-decoration: none;
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 12px;
+            }
+
+            .beritalainnya a:hover {
+                background-color: rgba(98, 139, 76, 0.75);
+                color: white;
+                text-decoration: none;
+            }
+
+            .testimoni .title {
+                font-family: 'Poetsen One', sans-serif;
+                font-size: 16px;
+            }
+        }
+
+        /* RESPONSIVE TABLET */
+        @media (max-width: 1270px) and (min-width: 601px) {
+            .isiabout img {
+                width: 40%;
+                height: 220px;
+                border-radius: 10px;
+            }
+
+            .card h4 {
+                font-weight: bold;
+                font-family: 'Poppins', sans-serif;
+                font-size: 20px;
+            }
+
+            .card img {
+                width: 50%;
+                margin-left: 45px;
+                margin-bottom: 10px;
+
+            }
+        }
+
+        @media (max-width: 820px) and (min-width: 601px){
+            .isiabout {
+                display: flex;
+                justify-content: space-between;
+                padding: 60px;
+            }
+
+            .isiabout .penjelasan {
+                width: 50%;
+                text-align: justify;
+                font-size: 12px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+            }
+
+            .isiabout img {
+                width: 40%;
+                height: 180px;
+                border-radius: 10px;
+            }
+
+            .isivisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .isimisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .column {
+                text-align: center;
+                width: 60%;
+                height: 60%;
+                padding: 20px;
+            }
+
+            .programunggulan {
+                margin: 0;
+                display: flex;
+                place-items: center;
+                margin-top: 60px;
+
+            }
+
+            .card {
+                padding: 30px;
+                text-align: center;
+                background-color: #FFFACD;
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                border-radius: 60px;
+            }
+
+            .card img {
+                width: 20%;
+                margin-left: 125px;
+                margin-bottom: 10px;
+
+            }
+
+            .card h4 {
+                font-weight: bold;
+                font-family: 'Poppins', sans-serif;
+                font-size: 20px;
+            }
+        }
+
+        @media (max-width: 768px) and (min-width: 601px){
+            .isiabout {
+                flex-direction: column;
+                place-items: center;
+                padding: 60px;
+            }
+
+            .isiabout .penjelasan {
+                width: 100%;
+                text-align: justify;
+                font-size: 14px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                margin-bottom: 20px;
+            }
+
+            .isiabout img {
+                width: 100%;
+                height: 240px;
+                border-radius: 10px;
+            }
+
+            .isivisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .visi {
+                padding: 50px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                background-color: rgba(255, 216, 117, 0.60);
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                text-align: justify;
+                margin-top: 40px;
+                border-radius: 15px;
+                font-size: 12px;
+            }
+
+            .visi h1 {
+                font-size: 14px;
+                font-family: 'Poetsen One', sans-serif;
+            }
+
+            .isimisi {
+                display: flex;
+                place-items: center;
+                width: 100%;
+                padding: 20px;
+            }
+
+            .misi {
+                padding: 50px;
+                font-family: 'Poppins', sans-serif;
+                font-weight: 500;
+                background-color: #FFFACD;
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                text-align: justify;
+                margin-top: 40px;
+                border-radius: 15px;
+                float: right;
+                font-size: 12px;
+            }
+
+            .misi h1 {
+                font-size: 14px;
+                font-family: 'Poetsen One', sans-serif;
+                float: right;
+            }
+
+            .column {
+                text-align: center;
+                width: 60%;
+                height: 60%;
+                padding: 20px;
+            }
+
+            .programunggulan {
+                margin: 0;
+                display: flex;
+                place-items: center;
+                margin-top: 60px;
+
+            }
+
+            .card {
+                padding: 30px;
+                text-align: center;
+                background-color: #FFFACD;
+                box-shadow: 0px 10px 4px rgba(0, 0, 0, 0.25);
+                border-radius: 60px;
+            }
+
+            .card img {
+                width: 20%;
+                margin-left: 110px;
+                margin-bottom: 10px;
+
+            }
+
+            .card h4 {
+                font-weight: bold;
+                font-family: 'Poppins', sans-serif;
+                font-size: 20px;
+            }
+        }
     </style>
 </head>
 
@@ -214,7 +600,7 @@
     @include ('header')
 
     <div class="judul">
-        <img style="width: 50%" src="{{ asset('images/home/logosdit.png') }}">
+        <img style="width: 50%" data-aos="zoom-in-up" src="{{ asset('images/home/logosdit.png') }}">
     </div>
     <div class="about">
         <div class="title">TENTANG SEKOLAH KAMI</div>
@@ -232,13 +618,13 @@
     <div class="visi-misi">
         <div class="title">VISI & MISI</div>
     </div>
-    <div class="isivisi">
+    <div class="isivisi" data-aos="zoom-in-up">
         <div class="visi">
             <h1>VISI</h1><br />
             Menjadi sekolah yang mampu melahirkan generasi yang islami, berkarakter, berbakat dan menguasai IPTEK
         </div>
     </div>
-    <div class="isimisi">
+    <div class="isimisi" data-aos="zoom-in-up">
         <div class="misi">
             <h1>MISI</h1><br />
             1. Menyelenggarakan pendidikan yang berbasis islami.<br />
@@ -251,37 +637,37 @@
         <div class="title">PROGRAM UNGGULAN</div>
     </div>
     <div class="programunggulan">
-        <div class="column">
+        <div class="column" data-aos="flip-left">
             <div class="card" style="background-color: rgba(255, 216, 117, 0.60); border-radius: 120px;">
                 <img src="{{ asset('images/home/kurikulum.png') }}">
                 <h4>KURIKULUM<br /><br /><br /></h4>
             </div>
         </div>
-        <div class="column">
+        <div class="column" data-aos="flip-right">
             <div class="card" style="background-color: #FFFACD; border-radius: 120px;">
-                <img src="{{ asset('images/home/guru.png') }}">
-                <h4>GURU TERSERTIFIKASI<br /><br /></h4>
+                <img src="{{ asset('images/home/fasilitas.png') }}">
+                <h4>FASILITAS<br /><br /><br /></h4>
             </div>
         </div>
     </div>
     <div class="programunggulan">
-        <div class="column">
+        <div class="column" data-aos="flip-left">
             <div class="card" style="background-color: #FFFACD; border-radius: 120px;">
                 <img src="{{ asset('images/home/metode.png') }}">
-                <h4>METODE PEMBELAJARAN<br /><br /></h4>
+                <h4>METODE <br />PEMBELAJARAN<br /><br /></h4>
             </div>
         </div>
-        <div class="column">
+        <div class="column" data-aos="flip-right">
             <div class="card" style="background-color: rgba(255, 216, 117, 0.60); border-radius: 120px;">
-                <img src="{{ asset('images/home/fasilitas.png') }}">
-                <h4>FASILITAS<br /><br /><br /></h4>
+                <img src="{{ asset('images/home/guru.png') }}">
+                <h4>GURU <br />TERSERTIFIKASI<br /><br /></h4>
             </div>
         </div>
     </div>
     <div class="berita">
         <div class="title">BERITA SEKOLAH</div>
     </div>
-    <div class="kartuberita">
+    <div class="kartuberita" data-aos="zoom-in-down">
         <div class="column">
             <div class="card" style="background-color: rgba(98, 139, 76, 0.75); color: white;">
                 <img src="">
@@ -311,8 +697,11 @@
         <div class="title">TESTIMONI</div>
     </div>
 
-        @include ('footer')
+    @include ('footer')
 
 </body>
+<script>
+    AOS.init();
+</script>
 
 </html>
