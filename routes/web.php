dd4a3aa7dbbf9a15b1d74bed72bbd1a06ce3e6f8
AnthonioMasbroo <129573492+AnthonioMasbroo@users.noreply.github.com>
2023-12-20 14:39:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\FasilitasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,18 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::get('/kegiatan', function () {
+    return view('kegiatan');
+});
+Route::get('/fasilitas', function () {
+    return view('fasilitas');
+});
+Route::get('/ppdb', function () {
+    return view('ppdb');
+});
+Route::get('/kontak', function () {
+    return view('kontak');
+});
 
 // ... (other routes)
 
@@ -33,19 +46,18 @@ Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
 Route::post('/submit-registration', [RegistrationController::class, 'store'])->name('submit.registration');
 Route::get('/registration-confirmation', [RegistrationController::class, 'showConfirmation'])->name('registration.confirmation');
 
 // Allow access to the admin dashboard without authentication
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    // Add other admin routes here
-    Route::put('/make-admin/{userId}', [AdminController::class, 'makeAdmin'])->name('admin.make-admin');
 });
 
 Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+Route::resource('/dashboard/berita', BeritaController::class)->middleware('auth');
+Route::resource('/dashboard/kegiatan', KegiatanController::class)->middleware('auth');
+Route::resource('/dashboard/fasilitas', FasilitasController::class)->middleware('auth');
 
 // Existing routes...
